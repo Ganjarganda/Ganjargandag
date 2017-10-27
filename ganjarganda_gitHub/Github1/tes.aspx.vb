ff52eb02e69840e9571d71010e539608b4963757
Ganjarganda <ganjar15ti@mahasiswa.pcr.ac.id>
2017-10-27 09:58:34 +0700

    ye

---
+++ ganjarganda_gitHub/Github1/tes.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class tes
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = "Selamat Datang haha"
+    End Sub
+
+End Class
---
+++ ganjarganda_gitHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="ganjarganda_gitHub.Global_asax" Language="vb" %>
